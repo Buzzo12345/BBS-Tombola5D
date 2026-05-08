897c97e01985cca1c49c7a114f4289addac562bc
Andrea Bambagiotti <bambagiotti.andrea@franchettisalviani.net>
2026-05-08 09:44:53 +0200

    cambiamnto

--- ServerRest/JsonSyntaxException.java
+++
@@ -1,9 +0,0 @@
-public class JsonSyntaxException extends RuntimeException {
-    public JsonSyntaxException(String message) {
-        super(message);
-    }
-
-    public JsonSyntaxException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
